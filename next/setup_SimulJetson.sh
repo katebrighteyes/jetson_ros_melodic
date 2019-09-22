3053a1e1636caeeb2d2c1e9cca967c2a100d8987
Kate <49902595+katebrighteyes@users.noreply.github.com>
2019-09-22 17:03:51 +0900

    Add files via upload

---
+++ next/setup_SimulJetson.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install ros-melodic-ar-track-alvar-msgs
+
+sudo apt autoremove
+
+sudo apt-get install ros-melodic-turtlebot3 -y
+
+sudo apt-get install ros-melodic-joy ros-melodic-teleop-twist-joy ros-melodic-teleop-twist-keyboard ros-melodic-laser-proc ros-melodic-rgbd-launch ros-melodic-depthimage-to-laserscan ros-melodic-rosserial-arduino ros-melodic-rosserial-python ros-melodic-rosserial-server ros-melodic-rosserial-client ros-melodic-rosserial-msgs ros-melodic-amcl ros-melodic-map-server ros-melodic-move-base ros-melodic-urdf ros-melodic-xacro ros-melodic-compressed-image-transport ros-melodic-rqt-image-view ros-melodic-gmapping ros-melodic-navigation ros-melodic-interactive-markers -y
+
+sudo apt-get install ros-melodic-turtlebot3-* -y
+sudo apt-get install ros-melodic-gazebo -y
+sudo apt-get install ros-melodic-turtlebot3-gazebo -y
+sudo apt-get install ros-melodic-gazebo-ros-pkgs ros-melodic-ros-control -y
+sudo apt install ros-melodic-slam-gmapping -y
+
+sudo apt-get install ros-melodic-gazebo-ros -y
+sudo apt-get install ros-melodic-gazebo-dev -y
+sudo apt-get install ros-melodic-gazebo-msgs -y
