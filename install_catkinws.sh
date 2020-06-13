f1255d30aeb0eaf4c04229893737cbe3fadcdb93
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-06-13 15:44:56 +0900

    Update and rename install_melodic_catkinws.sh to install_catkinws.sh

--- install_catkinws.sh
+++ install_catkinws.sh
@@ -2,7 +2,6 @@
 # Create a Catkin Workspace and setup ROS environment variables
 # Usage setupCatkinWorkspace.sh dirName
 
-source /opt/ros/melodic/setup.bash
 DEFAULTDIR=~/catkin_ws
 CLDIR="$1"
 if [ ! -z "$CLDIR" ]; then 
